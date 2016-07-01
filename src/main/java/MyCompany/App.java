0e2a26907334ef59b3d79301215a7995c0ff7fe9
root <root@ip-172-31-21-239.us-west-2.compute.internal>
2016-06-30 22:52:09 -0400

    myproject

---
+++ src/main/java/MyCompany/App.java
@@ -0,0 +1,14 @@
+package MyCompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
